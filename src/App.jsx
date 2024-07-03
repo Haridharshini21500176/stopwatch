d83bdb3c3df73915c11e3b4f5ba2c07ceaba018b
Haridharshini21500176 <vsvel79@gmail.com>
2024-07-03 10:17:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Stopwatch from './stopwatch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Stopwatch</h1>
+        <Stopwatch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
